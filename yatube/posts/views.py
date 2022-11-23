9afa6c2988f9fc6dc91dbb4006597bb00dae63c7
Никита Остапенко <nikitaostapenko0968@gmail.com>
2022-11-23 19:58:28 +0500

    1

---
+++ yatube/posts/views.py
@@ -0,0 +1,101 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+from django.core.paginator import Paginator
+
+from .forms import PostForm
+from .models import Group, Post, User
+
+
+NUM_OF_POSTS = 10
+
+def index(request):
+    text = 'Последние обновления на сайте'
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, NUM_OF_POSTS)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj, 'text': text}
+    template = 'posts/index.html'
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    post_list = group.posts.all()
+    paginator = Paginator(post_list, NUM_OF_POSTS)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+        'group': group,
+    }
+    return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    profile = get_object_or_404(User, username=username)
+    post_list = (
+        Post.objects.select_related("author", "group")
+        .filter(author=profile).all()
+    )
+    posts_count = post_list.count()
+    paginator = Paginator(post_list, NUM_OF_POSTS)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'profile': profile,
+        'posts_count': posts_count,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    """Страница поста и количество постов пользователя."""
+
+    post = get_object_or_404(Post, pk=post_id)
+
+    template = "posts/post_detail.html"
+    context = {"post": post}
+
+    return render(request, template, context)
+
+
+@login_required(login_url="users:login")
+def post_create(request):
+    """Добавления поста."""
+
+    template = "posts/create_post.html"
+
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.author = request.user
+        instance.save()
+        return redirect("posts:profile", request.user)
+
+    return render(request, template, {"form": form})
+
+
+@login_required(login_url="users:login")
+def post_edit(request, post_id):
+    """Редактирование поста. Доступно только автору."""
+
+    template = "posts/create_post.html"
+
+    post = get_object_or_404(Post, pk=post_id)
+
+    # Если редактировать пытается не автор
+    if request.user.id != post.author.id:
+        return redirect("posts:post_detail", post.pk)
+
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect("posts:post_detail", post.id)
+
+    context = {
+        "form": form,
+        "is_edit": True,
+    }
+    return render(request, template, context)
